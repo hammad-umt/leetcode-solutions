86baa7ced4e537e8e0ac6544f6b462c556eeaaca
Hammad Ur Rehman <155807057+hammad-umt@users.noreply.github.com>
2026-09-14 08:11:50 +0200

    Time: 0 ms (100.00%) | Memory: 43.3 MB (60.31%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        
+        while (temp != null && temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next; 
+            } else {
+                temp = temp.next; 
+            }
+        }
+        
+        return head;
+    }
+}
